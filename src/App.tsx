514e1edae9dc5f641df22465df001426d73f6e42
芹澤 <54226991+byrschcrm@users.noreply.github.com>
2024-07-24 23:53:04 +0900

    タスク削除ボタン作成

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ import dayjs from "dayjs";
 function App() {
   const [tasks, setTasks] = useState(getTasks())
 
+  const deleteTask = (id: any) => {
+    setTasks((prevTasks) => {
+      return prevTasks.filter((task) => task.id !== id)
+    })
+  }
+
   return (
     <>
       <div >
@@ -16,16 +22,16 @@ function App() {
       <div >
         TaskList
       </div>
-      <TaskList tasks={tasks} />
+      <TaskList tasks={tasks} deleteTask={deleteTask} />
     </>
   );
 }
 
 const getTasks = () => {
   return [
-    { name: 'タスク1', start_date: dayjs('2024-07-22 10:00:00'), end_date: dayjs('2024-07-22 10:30:00') },
-    { name: 'タスク2', start_date: dayjs('2024-07-24 09:00:00'), end_date: dayjs('2024-07-24 10:00:00') },
-    { name: 'タスク3', start_date: dayjs('2024-07-27 11:30:00'), end_date: dayjs('2024-07-27 13:00:00') },
+    { id: 1, name: 'タスク1', start_date: dayjs('2024-07-22 10:00:00'), end_date: dayjs('2024-07-22 10:30:00') },
+    { id: 2, name: 'タスク2', start_date: dayjs('2024-07-24 09:00:00'), end_date: dayjs('2024-07-24 10:00:00') },
+    { id: 3, name: 'タスク3', start_date: dayjs('2024-07-27 11:30:00'), end_date: dayjs('2024-07-27 13:00:00') },
   ]
 }
 
--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -3,22 +3,25 @@ import { FC } from 'react';
 
 type Props = {
     tasks: any[]
+    deleteTask: any
 }
 
 const TaskList: FC<Props> = (props) => {
-    const { tasks } = props
+    const { tasks, deleteTask } = props
 
     return (
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-4">
             <div className="border border-gray-400 h-5 text-center">名前</div>
             <div className="border border-gray-400 h-5 text-center">開始時刻</div>
             <div className="border border-gray-400 h-5 text-center">終了時刻</div>
+            <div className="border border-gray-400 h-5 text-center">削除</div>
             {
-                tasks.flatMap((t) =>
+                tasks.flatMap((task) =>
                     <>
-                        <div className="border border-gray-400 h-5" contentEditable={true}>{t.name}</div>
-                        <div className="border border-gray-400 h-5" contentEditable={true}>{t.start_date.format("YYYY-MM-DD HH:mm:ss")}</div>
-                        <div className="border border-gray-400 h-5" contentEditable={true}>{t.end_date.format("YYYY-MM-DD HH:mm:ss")}</div>
+                        <div className="border border-gray-400 h-5" contentEditable={true}>{task.name}</div>
+                        <div className="border border-gray-400 h-5" contentEditable={true}>{task.start_date.format("YYYY-MM-DD HH:mm:ss")}</div>
+                        <div className="border border-gray-400 h-5" contentEditable={true}>{task.end_date.format("YYYY-MM-DD HH:mm:ss")}</div>
+                        <div className="bg-red-500 border border-gray-400 cursor-pointer h-5" onClick={() => deleteTask(task.id)}></div>
                     </>
                 )
             }
