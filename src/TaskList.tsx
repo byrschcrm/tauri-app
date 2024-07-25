9b5d1ac6eda5e437303c78b2e7b0eb99eacce38e
芹澤 <54226991+byrschcrm@users.noreply.github.com>
2024-07-25 23:53:17 +0900

    微修正

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -11,17 +11,17 @@ const TaskList: FC<Props> = (props) => {
 
     return (
         <div className="grid grid-cols-4">
-            <div className="border border-gray-400 h-5 text-center">名前</div>
-            <div className="border border-gray-400 h-5 text-center">開始時刻</div>
-            <div className="border border-gray-400 h-5 text-center">終了時刻</div>
-            <div className="border border-gray-400 h-5 text-center">削除</div>
+            <div className="border border-gray-400 text-center">名前</div>
+            <div className="border border-gray-400 text-center">開始時刻</div>
+            <div className="border border-gray-400 text-center">終了時刻</div>
+            <div className="border border-gray-400 text-center">削除</div>
             {
                 tasks.flatMap((task) =>
                     <>
-                        <div className="border border-gray-400 h-5" contentEditable={true}>{task.name}</div>
-                        <div className="border border-gray-400 h-5" contentEditable={true}>{task.start_date?.format("YYYY-MM-DD HH:mm:ss") ?? ''}</div>
-                        <div className="border border-gray-400 h-5" contentEditable={true}>{task.end_date?.format("YYYY-MM-DD HH:mm:ss") ?? ''}</div>
-                        <div className="bg-red-500 border border-gray-400 cursor-pointer h-5" onClick={() => deleteTask(task.id)}></div>
+                        <div className="border border-gray-400" contentEditable={true}>{task.name}</div>
+                        <div className="border border-gray-400" contentEditable={true}>{task.start_date?.format("YYYY-MM-DD HH:mm:ss") ?? ''}</div>
+                        <div className="border border-gray-400" contentEditable={true}>{task.end_date?.format("YYYY-MM-DD HH:mm:ss") ?? ''}</div>
+                        <div className="bg-red-500 border border-gray-400 cursor-pointer" onClick={() => deleteTask(task.id)}></div>
                     </>
                 )
             }
