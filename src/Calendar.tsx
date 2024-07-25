49fcd65353e15fcd3f6b7f1aa120195f8a865dd9
芹澤 <54226991+byrschcrm@users.noreply.github.com>
2024-07-25 23:52:27 +0900

    カレンダー上のタスクセルを結合

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -15,118 +15,93 @@ const Calendar: FC<Props> = (props) => {
 
     return (
         <div className="grid grid-flow-col grid-rows-10">
-            <div className="flex h-10 items-center justify-end row-span-2 text-right">09:00</div>
-            <div className="flex h-10 items-center justify-end row-span-2 text-right">10:00</div>
-            <div className="flex h-10 items-center justify-end row-span-2 text-right">11:00</div>
-            <div className="flex h-10 items-center justify-end row-span-2 text-right">12:00</div>
-            <div className="flex h-10 items-center justify-end row-span-2 text-right">13:00</div>
+            <div className="flex items-center justify-end row-span-2 text-right">09:00</div>
+            <div className="flex items-center justify-end row-span-2 text-right">10:00</div>
+            <div className="flex items-center justify-end row-span-2 text-right">11:00</div>
+            <div className="flex items-center justify-end row-span-2 text-right">12:00</div>
+            <div className="flex items-center justify-end row-span-2 text-right">13:00</div>
 
-            <div className="border border-gray-400 h-5 text-center">2024.07.21(日)</div>
-            {
-                [
-                    { from_date: '2024-07-21 09:00:00', to_date: '2024-07-21 09:30:00' },
-                    { from_date: '2024-07-21 09:30:00', to_date: '2024-07-21 10:00:00' },
-                    { from_date: '2024-07-21 10:00:00', to_date: '2024-07-21 10:30:00' },
-                    { from_date: '2024-07-21 10:30:00', to_date: '2024-07-21 11:00:00' },
-                    { from_date: '2024-07-21 11:00:00', to_date: '2024-07-21 11:30:00' },
-                    { from_date: '2024-07-21 11:30:00', to_date: '2024-07-21 12:00:00' },
-                    { from_date: '2024-07-21 12:00:00', to_date: '2024-07-21 12:30:00' },
-                    { from_date: '2024-07-21 12:30:00', to_date: '2024-07-21 13:00:00' },
-                    { from_date: '2024-07-21 13:00:00', to_date: '2024-07-21 13:30:00' },
-                ].map((dates) => <div className={`border border-gray-400 h-5 ${tasks.some((task) => task.start_date?.isSameOrBefore(dayjs(dates.from_date)) && task.end_date?.isSameOrAfter(dayjs(dates.to_date))) ? 'bg-teal-500' : ''}`}></div>)
-            }
+            <div className="border border-gray-400 text-center">2024.07.21(日)</div>
+            {createCellElements(tasks, createDateRanges('2024-07-21'))}
 
-            <div className="border border-gray-400 h-5 text-center">2024.07.22(月)</div>
-            {
-                [
-                    { from_date: '2024-07-22 09:00:00', to_date: '2024-07-22 09:30:00' },
-                    { from_date: '2024-07-22 09:30:00', to_date: '2024-07-22 10:00:00' },
-                    { from_date: '2024-07-22 10:00:00', to_date: '2024-07-22 10:30:00' },
-                    { from_date: '2024-07-22 10:30:00', to_date: '2024-07-22 11:00:00' },
-                    { from_date: '2024-07-22 11:00:00', to_date: '2024-07-22 11:30:00' },
-                    { from_date: '2024-07-22 11:30:00', to_date: '2024-07-22 12:00:00' },
-                    { from_date: '2024-07-22 12:00:00', to_date: '2024-07-22 12:30:00' },
-                    { from_date: '2024-07-22 12:30:00', to_date: '2024-07-22 13:00:00' },
-                    { from_date: '2024-07-22 13:00:00', to_date: '2024-07-22 13:30:00' },
-                ].map((dates) => <div className={`border border-gray-400 h-5 ${tasks.some((task) => task.start_date?.isSameOrBefore(dayjs(dates.from_date)) && task.end_date?.isSameOrAfter(dayjs(dates.to_date))) ? 'bg-teal-500' : ''}`}></div>)
-            }
+            <div className="border border-gray-400 text-center">2024.07.22(月)</div>
+            {createCellElements(tasks, createDateRanges('2024-07-22'))}
 
-            <div className="border border-gray-400 h-5 text-center">2024.07.23(火)</div>
-            {
-                [
-                    { from_date: '2024-07-23 09:00:00', to_date: '2024-07-23 09:30:00' },
-                    { from_date: '2024-07-23 09:30:00', to_date: '2024-07-23 10:00:00' },
-                    { from_date: '2024-07-23 10:00:00', to_date: '2024-07-23 10:30:00' },
-                    { from_date: '2024-07-23 10:30:00', to_date: '2024-07-23 11:00:00' },
-                    { from_date: '2024-07-23 11:00:00', to_date: '2024-07-23 11:30:00' },
-                    { from_date: '2024-07-23 11:30:00', to_date: '2024-07-23 12:00:00' },
-                    { from_date: '2024-07-23 12:00:00', to_date: '2024-07-23 12:30:00' },
-                    { from_date: '2024-07-23 12:30:00', to_date: '2024-07-23 13:00:00' },
-                    { from_date: '2024-07-23 13:00:00', to_date: '2024-07-23 13:30:00' },
-                ].map((dates) => <div className={`border border-gray-400 h-5 ${tasks.some((task) => task.start_date?.isSameOrBefore(dayjs(dates.from_date)) && task.end_date?.isSameOrAfter(dayjs(dates.to_date))) ? 'bg-teal-500' : ''}`}></div>)
-            }
+            <div className="border border-gray-400 text-center">2024.07.23(火)</div>
+            {createCellElements(tasks, createDateRanges('2024-07-23'))}
 
-            <div className="border border-gray-400 h-5 text-center">2024.07.24(水)</div>
-            {
-                [
-                    { from_date: '2024-07-24 09:00:00', to_date: '2024-07-24 09:30:00' },
-                    { from_date: '2024-07-24 09:30:00', to_date: '2024-07-24 10:00:00' },
-                    { from_date: '2024-07-24 10:00:00', to_date: '2024-07-24 10:30:00' },
-                    { from_date: '2024-07-24 10:30:00', to_date: '2024-07-24 11:00:00' },
-                    { from_date: '2024-07-24 11:00:00', to_date: '2024-07-24 11:30:00' },
-                    { from_date: '2024-07-24 11:30:00', to_date: '2024-07-24 12:00:00' },
-                    { from_date: '2024-07-24 12:00:00', to_date: '2024-07-24 12:30:00' },
-                    { from_date: '2024-07-24 12:30:00', to_date: '2024-07-24 13:00:00' },
-                    { from_date: '2024-07-24 13:00:00', to_date: '2024-07-24 13:30:00' },
-                ].map((dates) => <div className={`border border-gray-400 h-5 ${tasks.some((task) => task.start_date?.isSameOrBefore(dayjs(dates.from_date)) && task.end_date?.isSameOrAfter(dayjs(dates.to_date))) ? 'bg-teal-500' : ''}`}></div>)
-            }
+            <div className="border border-gray-400 text-center">2024.07.24(水)</div>
+            {createCellElements(tasks, createDateRanges('2024-07-24'))}
 
-            <div className="border border-gray-400 h-5 text-center">2024.07.25(木)</div>
-            {
-                [
-                    { from_date: '2024-07-25 09:00:00', to_date: '2024-07-25 09:30:00' },
-                    { from_date: '2024-07-25 09:30:00', to_date: '2024-07-25 10:00:00' },
-                    { from_date: '2024-07-25 10:00:00', to_date: '2024-07-25 10:30:00' },
-                    { from_date: '2024-07-25 10:30:00', to_date: '2024-07-25 11:00:00' },
-                    { from_date: '2024-07-25 11:00:00', to_date: '2024-07-25 11:30:00' },
-                    { from_date: '2024-07-25 11:30:00', to_date: '2024-07-25 12:00:00' },
-                    { from_date: '2024-07-25 12:00:00', to_date: '2024-07-25 12:30:00' },
-                    { from_date: '2024-07-25 12:30:00', to_date: '2024-07-25 13:00:00' },
-                    { from_date: '2024-07-25 13:00:00', to_date: '2024-07-25 13:30:00' },
-                ].map((dates) => <div className={`border border-gray-400 h-5 ${tasks.some((task) => task.start_date?.isSameOrBefore(dayjs(dates.from_date)) && task.end_date?.isSameOrAfter(dayjs(dates.to_date))) ? 'bg-teal-500' : ''}`}></div>)
-            }
+            <div className="border border-gray-400 text-center">2024.07.25(木)</div>
+            {createCellElements(tasks, createDateRanges('2024-07-25'))}
 
-            <div className="border border-gray-400 h-5 text-center">2024.07.26(金)</div>
-            {
-                [
-                    { from_date: '2024-07-26 09:00:00', to_date: '2024-07-26 09:30:00' },
-                    { from_date: '2024-07-26 09:30:00', to_date: '2024-07-26 10:00:00' },
-                    { from_date: '2024-07-26 10:00:00', to_date: '2024-07-26 10:30:00' },
-                    { from_date: '2024-07-26 10:30:00', to_date: '2024-07-26 11:00:00' },
-                    { from_date: '2024-07-26 11:00:00', to_date: '2024-07-26 11:30:00' },
-                    { from_date: '2024-07-26 11:30:00', to_date: '2024-07-26 12:00:00' },
-                    { from_date: '2024-07-26 12:00:00', to_date: '2024-07-26 12:30:00' },
-                    { from_date: '2024-07-26 12:30:00', to_date: '2024-07-26 13:00:00' },
-                    { from_date: '2024-07-26 13:00:00', to_date: '2024-07-26 13:30:00' },
-                ].map((dates) => <div className={`border border-gray-400 h-5 ${tasks.some((task) => task.start_date?.isSameOrBefore(dayjs(dates.from_date)) && task.end_date?.isSameOrAfter(dayjs(dates.to_date))) ? 'bg-teal-500' : ''}`}></div>)
-            }
+            <div className="border border-gray-400 text-center">2024.07.26(金)</div>
+            {createCellElements(tasks, createDateRanges('2024-07-26'))}
 
-            <div className="border border-gray-400 h-5 text-center">2024.07.27(土)</div>
-            {
-                [
-                    { from_date: '2024-07-27 09:00:00', to_date: '2024-07-27 09:30:00' },
-                    { from_date: '2024-07-27 09:30:00', to_date: '2024-07-27 10:00:00' },
-                    { from_date: '2024-07-27 10:00:00', to_date: '2024-07-27 10:30:00' },
-                    { from_date: '2024-07-27 10:30:00', to_date: '2024-07-27 11:00:00' },
-                    { from_date: '2024-07-27 11:00:00', to_date: '2024-07-27 11:30:00' },
-                    { from_date: '2024-07-27 11:30:00', to_date: '2024-07-27 12:00:00' },
-                    { from_date: '2024-07-27 12:00:00', to_date: '2024-07-27 12:30:00' },
-                    { from_date: '2024-07-27 12:30:00', to_date: '2024-07-27 13:00:00' },
-                    { from_date: '2024-07-27 13:00:00', to_date: '2024-07-27 13:30:00' },
-                ].map((dates) => <div className={`border border-gray-400 h-5 ${tasks.some((task) => task.start_date?.isSameOrBefore(dayjs(dates.from_date)) && task.end_date?.isSameOrAfter(dayjs(dates.to_date))) ? 'bg-teal-500' : ''}`}></div>)
-            }
+            <div className="border border-gray-400 text-center">2024.07.27(土)</div>
+            {createCellElements(tasks, createDateRanges('2024-07-27'))}
         </div>
     )
+}
+
+const createCellElements = (tasks: any[], dateRanges: any) => {
+    const cell_ids = dateRanges.map((dateRange: any, idx: number) => tasks.find((task) => task.start_date?.isSameOrBefore(dateRange.from) && task.end_date?.isSameOrAfter(dateRange.to))?.id ?? -idx)
+
+    const row_spans: any[] = []
+    let prev_id = 0
+    let cur_row_span = 0
+    for (let i = 0; i < cell_ids.length; i++) {
+        const cell_id = cell_ids[i]
+        if (i === 0) {
+            prev_id = cell_id
+            cur_row_span = cell_id > 0 ? 1 : 0
+        } else if (cell_id === prev_id) {
+            cur_row_span += 1
+        } else {
+            row_spans.push(cur_row_span)
+            prev_id = cell_id
+            cur_row_span = cell_id > 0 ? 1 : 0
+        }
+        if (i === cell_ids.length - 1) {
+            row_spans.push(cur_row_span)
+        }
+    }
+
+    return row_spans.map((e) => <div className={`border border-gray-400 ${e > 0 ? `${getRowSpan(e)} bg-teal-500` : ''}`}></div>)
+}
+
+const createDateRanges = (ymd: string) => {
+    return [
+        { from: dayjs(`${ymd} 09:00:00`), to: dayjs(`${ymd} 09:30:00`) },
+        { from: dayjs(`${ymd} 09:30:00`), to: dayjs(`${ymd} 10:00:00`) },
+        { from: dayjs(`${ymd} 10:00:00`), to: dayjs(`${ymd} 10:30:00`) },
+        { from: dayjs(`${ymd} 10:30:00`), to: dayjs(`${ymd} 11:00:00`) },
+        { from: dayjs(`${ymd} 11:00:00`), to: dayjs(`${ymd} 11:30:00`) },
+        { from: dayjs(`${ymd} 11:30:00`), to: dayjs(`${ymd} 12:00:00`) },
+        { from: dayjs(`${ymd} 12:00:00`), to: dayjs(`${ymd} 12:30:00`) },
+        { from: dayjs(`${ymd} 12:30:00`), to: dayjs(`${ymd} 13:00:00`) },
+        { from: dayjs(`${ymd} 13:00:00`), to: dayjs(`${ymd} 13:30:00`) },
+    ]
+}
+
+const getRowSpan = (num: any) => {
+    switch (num) {
+        case 1:
+            return 'row-span-1'
+        case 2:
+            return 'row-span-2'
+        case 3:
+            return 'row-span-3'
+        case 4:
+            return 'row-span-4'
+        case 5:
+            return 'row-span-5'
+        case 6:
+            return 'row-span-6'
+        default:
+            return ''
+    }
 }
 
 export default Calendar
