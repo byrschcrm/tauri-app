9d09fbbb5886af1832cfab0e6e7beb4a158bbc29
芹澤 <54226991+byrschcrm@users.noreply.github.com>
2024-07-26 19:55:50 +0900

    日付をセレクトボックス表示に

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -19,14 +19,47 @@ const TaskList: FC<Props> = (props) => {
                 tasks.flatMap((task) =>
                     <>
                         <div className="border border-gray-400" contentEditable={true}>{task.name}</div>
-                        <div className="border border-gray-400" contentEditable={true}>{task.start_date?.format("YYYY-MM-DD HH:mm:ss") ?? ''}</div>
-                        <div className="border border-gray-400" contentEditable={true}>{task.end_date?.format("YYYY-MM-DD HH:mm:ss") ?? ''}</div>
+                        <div className="border border-gray-400" contentEditable={true}>
+                            <select>{createDateOptions(task.start_date)}</select>
+                        </div>
+                        <div className="border border-gray-400" contentEditable={true}>
+                            <select>{createDateOptions(task.end_date)}</select>
+                        </div>
                         <div className="bg-red-500 border border-gray-400 cursor-pointer" onClick={() => deleteTask(task.id)}></div>
                     </>
                 )
             }
         </div>
     )
+}
+
+const createDateOptions = (date: any) => [<option></option>].concat(createYmdPatterns().flatMap((ymd) => createHmsPatterns().map((hms) => <option selected={date.isSame(`${ymd}${hms}`)}>{`${ymd} ${hms}`}</option>)))
+
+const createYmdPatterns = () => {
+    return [
+        '2024-07-21',
+        '2024-07-22',
+        '2024-07-23',
+        '2024-07-24',
+        '2024-07-25',
+        '2024-07-26',
+        '2024-07-27',
+    ]
+}
+
+const createHmsPatterns = () => {
+    return [
+        '09:00:00',
+        '09:30:00',
+        '10:00:00',
+        '10:30:00',
+        '11:00:00',
+        '11:30:00',
+        '12:00:00',
+        '12:30:00',
+        '13:00:00',
+        '13:30:00',
+    ]
 }
 
 export default TaskList
